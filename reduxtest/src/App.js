578a2b8d221439ae8d0adb3b1a0860becc81141b
Javier Repilado <javier.repilado@Javiers-MacBook-Pro.local>
2021-07-01 12:58:11 +0200

    add basic store

---
+++ reduxtest/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { createStore } from 'redux';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+const hello = () => ({ welcome: 'hello' });
+const store = createStore(hello);
+console.log(store.getState());
