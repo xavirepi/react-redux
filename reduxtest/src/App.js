42ba112deb84fffedd47aef5643265f94b89b7ec
Javier Repilado <javier.repilado@Javiers-MacBook-Pro.local>
2021-07-01 13:26:20 +0200

    create basic reducer and basic action dispatched

--- reduxtest/src/App.js
+++ reduxtest/src/App.js
@@ -26,7 +26,33 @@ function App() {
 
 export default App;
 
+const defaultState = {
+  welcome: 'Hi',
+  otherState: 'Some stuff',
+  anotherState: 'Some other stuff'
+}
 
-const hello = () => ({ welcome: 'hello' });
-const store = createStore(hello);
+// Reducer
+const greeting = (state = defaultState, action) => { // If state is passed as an argument use state, otherwise use default state.
+  // Supose we have a window toggle
+  switch (action.type) {
+    case 'GREET_ME':
+      return {  ...state, welcome: 'Hello Javier' };
+    case 'GREET_WORLD':
+      return { ...state, welcome: 'Hello World' };
+    default: 
+      return state;
+  }
+  // An action is an object (decriber) - Dispatch --> Go do what the actions says
+};
+
+const store = createStore(greeting);
 console.log(store.getState());
+
+// Action
+store.dispatch({ // Dispatch is a method of store 
+  // Actions can include data, a payload when calling an API or all sort of stuff
+  type: 'GREET_ME'
+})
+
+console.log('state:', store.getState());
